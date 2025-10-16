670e481a06690936b1604b63e6c693e939a2704d
thesnake94 <snkrs.dayen@gmail.com>
2025-10-16 11:26:16 +0200

    ajout du fichier test

---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+from app import app
+
+def test_health_route():
+    client = app.test_client()
+    r = client.get("/health")
+    assert r.status_code == 200
+    assert r.get_json().get("health") == "pass"
